a92c255577692e2fc336e834d2f82499fe35fd08
HaniaSadiq13 <126976598+HaniaSadiq13@users.noreply.github.com>
2023-12-22 15:20:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+
+def read_dataset():
+    # Your code to read the dataset from an online/local source
+    pass
+
+if __name__ == "__main__":
+    read_dataset()
---
+++ plot.py
@@ -0,0 +1,12 @@
+# plot.py
+import matplotlib.pyplot as plt
+
+def plot_chart(data):
+    # Your code to plot the chart based on the provided data
+    pass
+
+if __name__ == "__main__":
+    # Example data
+    data = [1, 2, 3, 4, 5]
+    plot_chart(data)
+    plt.show()
